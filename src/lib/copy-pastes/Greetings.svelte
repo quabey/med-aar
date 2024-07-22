6901c9a8dd44b619186237588ee41c054cc00b7b
quabey <50772988+quabey@users.noreply.github.com>
2024-07-22 23:14:26 +0200

    Pastes

--- src/lib/copy-pastes/Greetings.svelte
+++ src/lib/copy-pastes/Greetings.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Section from '$lib/copy-pastes/section.svelte';
+	import Section from '$lib/copy-pastes/Section.svelte';
 	import greetings from '$lib/copy-pastes/pastes/greetingsPastes.json';
 	import Paste from '$lib/copy-pastes/Paste.svelte';
 </script>
