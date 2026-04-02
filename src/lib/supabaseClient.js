c7f8afb878255cd11a57b8a3c9a62acab63b3577
quabey <50772988+quabey@users.noreply.github.com>
2026-04-02 22:26:30 +0200

    refactor: update Supabase client import to use dynamic environment variables

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createBrowserClient } from '@supabase/ssr';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
-export const supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createBrowserClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
